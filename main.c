fa0499bd2cd37f62dc1a094fc102ff7508498f58
Elon-Maks <48629557+Elon-Maks@users.noreply.github.com>
2019-10-27 00:00:34 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void formfile()
+{
+    FILE *f;
+    f=fopen("file.bin","w");
+    srand(time(0));
+    int n=rand()%10+5;
+    for (int i=1;i<=n;i++)
+    {
+        int k=rand()%10+1;
+        for (int j=1;j<=k;j++)
+        {
+            char c=rand()%26+97;
+            fwrite(&c,sizeof(c),1,f);
+        }
+        char c=10;
+        fwrite(&c,sizeof(c),1,f);
+    }
+    fclose(f);
+}
+
+void readfile()
+{
+    FILE *f;
+    f=fopen("file.bin","r");
+    char c;
+    while (fread(&c,sizeof(c),1,f))
+        printf("%c",c);
+    
+}
+
+void sortfile()
+{
+   
+}
+
+int main()
+{
+    formfile();
+    readfile();
+    sortfile();
+    printf("\n");
+    readfile();
+    return 0;
+}
